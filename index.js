1c53fedc3b19321d7204060b7b00ee75e1ac4f3f
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2026-03-31 11:39:17 +0000

    Serve Email

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-var { registerTestRoutes } = require('w4j-test-utils');
 app.use(cors({optionsSuccessStatus: 200}));
 app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+var { registerTestRoutes } = require('w4j-test-utils');
+registerTestRoutes(app);
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
@@ -43,7 +44,8 @@ Your code Goes Here
 
 
 
-registerTestRoutes(app);
+
+
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
